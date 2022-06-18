f0a90692da288691d7557facd55f25fcc237655a
João Marcos <jottasintonia@gmail.com>
2022-06-17 22:03:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 let btnStart = document.getElementById("start")
+let mili = document.getElementById("mili")
+let segundos = document.getElementById("segundos")
+let minutos = document.getElementById("minutos")
+let horas = document.getElementById("horas")
 
-console.log("1")
+
+let startTimer
+let ms = 0
+let seg = 0
+let min = 0
+let hr = 0
 
 function start(){
-  console.log("2")
+  startTimer = setInterval(function () {
+    ms++
+
+    if(ms === 100){
+      seg++
+      ms = 0 
+    }
+
+    if(seg ==60){
+      min++
+      seg = 0
+    }
+
+    if()
+
+    atualizarValor()
+  }, 10);
+
+  atualizaValor()
 }
 
-console.log("3")
+
+
+function atualizaValor(){
+  mili.innerHTML = ms
+  segundos.innerHTML = seg
+  minutos.innerHTML = min
+}
+
+btnStart.addEventListener("click", start)
